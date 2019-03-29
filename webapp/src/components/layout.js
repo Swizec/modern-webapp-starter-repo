b29170466e564e158ee328050147fa9fbf11d34e
Swizec Teller <swizec@swizec.com>
2019-03-29 13:30:58 -0700

    Reakit provider and Grid

--- webapp/src/components/layout.js
+++ webapp/src/components/layout.js
@@ -26,24 +26,27 @@ const Layout = ({ children }) => (
       }
     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
+      <Provider theme={theme}>
+        <Grid templateRows="120px 1fr 20px" style={{ minHeight: "700px" }}>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          {/* Change these elements to Grid.Item and use CenterColumn inside for centering */}
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()}, Built with
+              {` `}
+              <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        </Grid>
+      </Provider>
     )}
   />
 )
